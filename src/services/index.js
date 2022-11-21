251424d758fb9b8808e8ef5de4ecf07f351a41b3
benoitgrasset <32497923+benoitgrasset@users.noreply.github.com>
2022-11-21 15:39:11 +0100

    1 file modified

--- src/services/index.js
+++ src/services/index.js
@@ -13,6 +13,21 @@ export const getMessages = async (conversationId) => {
   return messages;
 };
 
+export const postMessage = async (conversationId, body) => {
+  await fetch(baseUrl + `messages/${conversationId}`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(body),
+  }).catch((err) =>
+    console.error(
+      `Error posting message from conversation ${conversationId}`,
+      err
+    )
+  );
+};
+
 export const deleteMessage = async (messageId) => {
   await fetch(baseUrl + `message/${messageId}`, {
     method: 'DELETE',
